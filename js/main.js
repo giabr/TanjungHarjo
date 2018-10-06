0633f593847967a5d9c5ec53fb68eb0f933e998e
giabr <31702129+giabr@users.noreply.github.com>
2018-10-06 22:48:40 +0700

    qqw

--- js/main.js
+++ js/main.js
@@ -94,19 +94,19 @@ jQuery(document).ready(function( $ ) {
     }
   });
 
-  // Porfolio filter
-  $("#portfolio-flters li").click ( function() {
-    $("#portfolio-flters li").removeClass('filter-active');
+  // Katalog filter
+  $("#katalog-flters li").click ( function() {
+    $("#katalog-flters li").removeClass('filter-active');
     $(this).addClass('filter-active');
 
     var selectedFilter = $(this).data("filter");
-    $("#portfolio-wrapper").fadeTo(100, 0);
+    $("#katalog-wrapper").fadeTo(100, 0);
 
-    $(".portfolio-item").fadeOut().css('transform', 'scale(0)');
+    $(".katalog-item").fadeOut().css('transform', 'scale(0)');
 
     setTimeout(function() {
       $(selectedFilter).fadeIn(100).css('transform', 'scale(1)');
-      $("#portfolio-wrapper").fadeTo(300, 1);
+      $("#katalog-wrapper").fadeTo(300, 1);
     }, 300);
   });
 
